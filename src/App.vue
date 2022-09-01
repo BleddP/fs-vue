078d218e6ea5dab33487d0bd2daf9edc74beae60
Bleddyn Pijpers <bleddyn.pijpers@cloud-studio.co.uk>
2022-09-01 15:53:13 +0800

    basic styles

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <template>
   <div>
       <h1>Vue app</h1>
-      <Hello title="hello" />
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello.vue'
 
 export default {
-  name: 'App',
-  components: {
-    Hello
-  }
+  name: 'App'
 }
 </script>
 
