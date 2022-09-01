721c928ad80a6197afc68b64e14a85800333e94e
Bleddyn Pijpers <bleddyn.pijpers@cloud-studio.co.uk>
2022-09-01 15:27:47 +0800

    first commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Hello Component</h1>
+    <h4>{{ title }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hello",
+  props: {
+    title: String,
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// Styles
+import './styles/global.scss'
+
 createApp(App).mount('#app')
